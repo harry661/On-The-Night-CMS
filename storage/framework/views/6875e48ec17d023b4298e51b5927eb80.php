46f64dd3d0a9f622e77a2de44a1282a08c2ff46c
Harry <harry@Hollies-MacBook-Pro.local>
2025-10-06 16:09:43 +0100

    Fix inconsistent image URL structure - standardize all venue images to use /venues/ directory

---
+++ storage/framework/views/6875e48ec17d023b4298e51b5927eb80.php
@@ -0,0 +1,7 @@
+<img 
+    src="<?php echo e($src); ?>" 
+    alt="<?php echo e($alt); ?>" 
+    style="width: <?php echo e($size); ?>px; height: <?php echo e($size); ?>px; object-fit: cover; border-radius: 4px;"
+    onerror="this.src='/images/venue-placeholder.jpg'"
+/>
+<?php /**PATH /Users/harry/Documents/On the Night CMS/resources/views/filament/components/image-thumbnail.blade.php ENDPATH**/ ?>
